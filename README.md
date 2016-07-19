8c4624a6d9a6b25c02c24696c0cd71f4f7e30399
Josh Richard <where@theyconfuse.me>
2016-07-19 09:56:34 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# declawed-cat-lookup
+
+cathaven.org declawed cat lookup
+
+---
+
+### Abstract
+
+I am considering adopting a cat, and currently looking around the Baton Rouge, Louisiana area. [Cat Haven](https://cathaven.org) was the first one I came across and looks quite reputable, so I started my search.
+
+After considering my options, I realized a declawed cat would be ideal; [don't worry Peta](http://www.peta.org/living/companion-animals/8-reasons-never-declaw-cats/), I am not going to force that procedure on an animal. However, if a cat is currently up for adoption that was previously declawed, I would welcome the cat in to my indoor, furever home.
+
+Using my [full-stack dev[eloper]](http://codeup.com/what-is-a-full-stack-developer/) skills, I quickly built a [Web scraping](https://en.wikipedia.org/wiki/Web_scraping) application to search in detail for me.
+
+    npm install
+
+    npm start
+
+Running the node application lists the cat with their current `Declawed` status
+
+    Patches - No
+    Zoomy - No
+    Scout - No
+    Olive - No
+    Princess - No
+    Tiny - No
+    Dorian - No
+    ...
+
+It eventually times out, and I am stopping development there.. what have I done??
---
+++ app.js
@@ -0,0 +1,43 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+/**
+ * fetchCat
+ * Fetch cat information from parsed url
+ */
+var fetchCat = function(url) {
+    url = "http://www.petango.com/webservices/adoptablesearch/" + url;
+    request({
+        url: url,
+        headers: {
+            'User-Agent': 'c0nfus3d <+http://theyconfuse.me/online>'
+        }
+    }, function(err, resp, body) {
+        if (err) console.log(err);
+        $ = cheerio.load(body);
+
+        console.log(
+            $("#lbName").text() +
+            " - " +
+            $("#trDeclawed").text().replace(/[\r\n]/g, '').split("Declawed")[1].trim()
+        );
+    });
+};
+
+/** Load of available cats */
+var url = "http://www.petango.com/webservices/adoptablesearch/wsAdoptableAnimals.aspx?species=Cat&sex=A&agegroup=All&location=&site=&onhold=A&orderby=ID&colnum=3&css=http://cathaven.ehclients.com/assets/css/catlistings.css&authkey=13bdekqnmlov3usxre52it28sxrj8dp1ghkpbfex523ooi2rd3&recAmount=&detailsInPopup=Yes&featuredPet=Include&stageID=";
+
+request({
+    url: url,
+    headers: {
+        'User-Agent': 'c0nfus3d <+http://theyconfuse.me/online>'
+    }
+}, function(err, resp, body) {
+    if (err) console.log(err);
+    $ = cheerio.load(body);
+
+    $(".list-animal-name a").each(function() {
+        var url = $(this).attr('href').split("'")[1].split("&css=")[0];
+        fetchCat(url);
+    });
+});
